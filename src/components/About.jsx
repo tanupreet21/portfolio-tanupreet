a60af361613949dfb6549c6c21a93606780792bd
tanupreetkaur <tanubal21@gmail.com>
2026-03-22 14:24:40 -0400

    Fixed animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,8 @@ export default function About() {
                         whileHover={{ scale: 1.03 }}
                         initial={{ opacity: 0, y: 30}}
                         whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.5 }}
+                        viewport={{ once: true }}
+                        transition={{ duration: 0.5, delay: 0.1 }}
                     >
                         <div className="flex items-center mb-4 gap-3">
                         <FaBriefcase className="text-primary text-2xl" />
